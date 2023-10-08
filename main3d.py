a7906b6669e9db315b54621732a953473735e069
Tasnim Ferdous <tferdous611@gmail.com>
2023-10-08 12:18:27 -0400

    compute gradient for x,y and move in opposite direction of slope for z coord

--- main3d.py
+++ main3d.py
@@ -8,6 +8,7 @@ def z_function(x, y):
 
 
 def calculate_gradient(x, y):
+    # calculate derivatives of x, y
     return np.cos(5 * x) * np.cos(5 * y), -np.sin(5 * x) * np.sin(5 * y)
 
 
@@ -18,7 +19,15 @@ X, Y = np.meshgrid(x, y)
 
 Z = z_function(X, Y)
 
+current_pos = (0.7, 0.4, z_function(0.7, 0.4))
+learning_rate = 0.01
+
 ax = plt.subplot(projection='3d')
 
-ax.plot_surface(X, Y, Z, cmap="viridis")
-plt.show()
+for _ in range(1000):
+    X_derivative, Y_derivative = calculate_gradient(current_pos[0], current_pos[1])
+    X_new, Y_new = current_pos[0] - learning_rate * X_derivative, current_pos[1] - learning_rate * Y_derivative
+    current_pos = (X_new, Y_new, z_function(X_new, Y_new))
+
+    ax.plot_surface(X, Y, Z, cmap="viridis")
+
