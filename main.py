8daf28594a9c21d90c519cb4683f3222702a2c96
Tasnim Ferdous <tferdous611@gmail.com>
2023-10-08 11:54:08 -0400

    add explanatory comments for descent

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ current_pos = (80, y_function(80))
 learning_rate = 0.01
 
 for _ in range(1000):
+    # calculates new X coord by moving us towards the local min using a negative slope (-y_derivative)
     new_x = current_pos[0] - learning_rate * y_derivative(current_pos[0])
-    new_y = y_function(new_x)
-    current_pos = (new_x, new_y)
+    new_y = y_function(new_x) # new Y coord is simply x^2
+    current_pos = (new_x, new_y) # set new current_pos per iteration
 
 plt.plot(x, y)
 plt.scatter(current_pos[0], current_pos[1], color="red")
