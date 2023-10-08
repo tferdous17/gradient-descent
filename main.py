417d5cdf47098f811bd64c014e4258496528394c
Tasnim Ferdous <tferdous611@gmail.com>
2023-10-08 11:49:28 -0400

    graph a parabola and add a sample point for gradient descent

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ def y_function(x):
 
 def y_derivative(x):
     return 2 * x
+
+x = np.arange(-100, 100, 0.1)
+y = y_function(x)
+
+current_pos = (80, y_function(80))
+
+plt.plot(x, y)
+plt.scatter(current_pos[0], current_pos[1], color="red")
+plt.show()
