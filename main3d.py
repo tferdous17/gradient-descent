b465dbf85ccb778ba79546bf87f0d7d3a2329f2c
Tasnim Ferdous <tferdous611@gmail.com>
2023-10-08 12:11:23 -0400

    create meshgrid of x, y and plot 3d projection of x,y,z

--- main3d.py
+++ main3d.py
@@ -9,3 +9,16 @@ def z_function(x, y):
 
 def calculate_gradient(x, y):
     return np.cos(5 * x) * np.cos(5 * y), -np.sin(5 * x) * np.sin(5 * y)
+
+
+x = np.arange(-1, 1, 0.05)
+y = np.arange(-1, 1, 0.05)
+
+X, Y = np.meshgrid(x, y)
+
+Z = z_function(X, Y)
+
+ax = plt.subplot(projection='3d')
+
+ax.plot_surface(X, Y, Z, cmap="viridis")
+plt.show()
