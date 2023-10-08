bd90e0f17902329dd3288d2bd26f8108c2f55e51
Tasnim Ferdous <tferdous611@gmail.com>
2023-10-08 12:07:31 -0400

    implement calculate_gradient func to find partial derivatives of x, y

--- main3d.py
+++ main3d.py
@@ -5,3 +5,7 @@ import matplotlib.pyplot as plt
 def z_function(x, y):
     # fancy wave function
     return np.sin(5 * x) * np.cos(5 * y) / 5
+
+
+def calculate_gradient(x, y):
+    return np.cos(5 * x) * np.cos(5 * y), -np.sin(5 * x) * np.sin(5 * y)
